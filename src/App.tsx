8a494d415c07450b1c69708797332c559cc9575d
ReiRyuSTI <ry-tanaka@sios.com>
2023-01-10 17:03:47 +0900

    point

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { RouterConfig } from "./router/RouterConfig";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <RouterConfig />
     </div>
   );
 }
---
+++ src/router/RouterConfig.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+export const RouterConfig = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<>abc</>} />
+        <Route path="/test" element={<>test</>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
